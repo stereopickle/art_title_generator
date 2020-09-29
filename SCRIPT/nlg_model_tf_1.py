b5d5659fd8e1566ac072aac7d4805f3e2b40e583
stereopickle <stereopickle@gmail.com>
2020-09-29 02:27:21 +0000

    minor change

--- SCRIPT/nlg_model_tf_1.py
+++ SCRIPT/nlg_model_tf_1.py
@@ -15,15 +15,142 @@ import numpy as np
 import pickle
 
 import tensorflow as tf
-import keras
-from keras import backend as K
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Embedding, Input, LSTM, add
-from keras.optimizers import Adam
-from keras.utils import multi_gpu_model
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Embedding, Input, LSTM, add
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import multi_gpu_model, to_categorical
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-from sequence_generator import *
-from evaluation_tools import *
+def get_keys(dict_):
+    ''' 
+    Helper to return a list of keys 
+    given dictionary
+    '''
+    return list(dict_.keys())
+
+def get_vals(dict_):
+    ''' 
+    Helper to return a list of values 
+    given dictionary
+    '''
+    return list(dict_.values())
+    
+def get_features(features_dict, img_ind):
+    ''' 
+    Helper to return feature values 
+    given feature dictionary and index
+    '''
+    if isinstance(img_ind, list):
+        return [features_dict[x][0] for x in img_ind]
+    elif isinstance(img_ind, str):
+        return features_dict[img_ind][0]
+    else:
+        print('img_ind must be a list or string type')
+        return None
+    
+def get_text(dictionary, img_ind):
+    ''' 
+    Helper to return a list of description 
+    given an index 
+    '''
+    return dictionary[img_ind]    
+
+class sequence_generator:
+    ''' 
+    Returns a sequence_generator object
+    '''
+    def __init__(self, dictionary, features):
+        ''' INPUT: a dictionary of descriptions and features '''
+        self.dictionary = dictionary
+        self.features = features
+        self.img_index = get_keys(self.dictionary)
+        self.texts = get_vals(self.dictionary)
+
+    def update_selection(self, list_):
+        ''' 
+        INPUT: select list of image indices
+        Create selector, and subsets (select_dictionary, select_img_inds, select_texts)
+        '''
+        self.selector = list_
+        self.select_dictionary = {k: v for k, v in self.dictionary.items() if (k in list_) & (k in self.features)}
+        self.select_img_inds = get_keys(self.select_dictionary)
+        self.select_texts = get_vals(self.select_dictionary)
+    
+    def sequence_process(self, dict_):
+        ''' Helper to process breakdown on all select dictionary '''
+        X1, X2, Y = [], [], []
+
+        def breakdown_sequence(list_):
+            ''' Helper to return a list of breakdown sequences and the output '''
+            x, y = [], []
+            for i in range(1, len(list_)):
+                x.append(list_[:i])
+                y.append(list_[i])
+            return x, y
+        
+        for ind, texts in dict_.items():
+            sequences = self.tokenizer.texts_to_sequences(texts)
+            
+            for seq in sequences:
+                x, y = breakdown_sequence(seq)
+
+                X1.extend(np.repeat(ind, len(y)))
+                X2.extend(x)
+                Y.extend(y)
+
+        return X1, X2, Y
+
+    def train_generator(self, train_list):
+        '''
+        INPUT a list of training ids, 
+        RETURN image inputs, text inputs, and outputs
+        ASSIGN max_length and vocab size
+        '''
+        self.update_selection(train_list)
+
+        self.tokenizer = Tokenizer()
+        self.tokenizer.fit_on_texts(np.concatenate(self.select_texts))
+        self.num_vocab = len(self.tokenizer.word_index)+1
+        
+        dict_ = self.select_dictionary
+        
+
+        X1, X2, Y = self.sequence_process(dict_)
+        
+        X2 = pad_sequences(X2)
+        self.max_length = X2.shape[1]
+    
+        Y = to_categorical(Y, self.num_vocab)
+        X1 = get_features(self.features, X1)
+        
+        return np.array(X1), np.array(X2), np.array(Y)
+
+    def validation_generator(self, val_list):
+        '''
+        INPUT a list of validation ids, 
+        RETURN image inputs, text inputs and outputs
+        '''
+        self.update_selection(val_list)
+        
+        dict_ = self.select_dictionary
+
+        X1, X2, Y = self.sequence_process(dict_)
+        X2 = pad_sequences(X2, maxlen = self.max_length)
+        Y = to_categorical(Y, num_classes = self.num_vocab)
+        X1 = get_features(self.features, X1)
+
+        return np.array(X1), np.array(X2), np.array(Y)
+    
+    def get_num_vocab(self):
+        return self.num_vocab
+    def get_max_length(self):
+        return self.max_length
+    def get_tokenizer(self):
+        return self.tokenizer
+
+    
 
 if __name__ == '__main__':
         
